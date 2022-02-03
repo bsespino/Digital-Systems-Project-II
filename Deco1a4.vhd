91a6aeed1242f5f9545660c5cd1e9453e979861f
bsespino <97320131+bsespino@users.noreply.github.com>
2022-02-02 22:04:57 -0500

    Update Deco1a4.vhd
    Digital Systems 2
    Names: Chanalata Charlotte, Leones Daniel y Espinosa Bryan
    This block takes a vector of n bits and divides it into the desired vectors in order to encode a signal from a binary stored database.

--- Deco1a4.vhd
+++ Deco1a4.vhd
@@ -1,3 +1,7 @@
+--Digital Systems 2
+--Names: Chanalata Charlotte, Leones Daniel y Espinosa Bryan
+--This block takes a vector of n bits and divides it into the desired vectors in order to encode a signal from a binary stored database.
+
 --Library
 library ieee;
 use ieee.std_logic_1164.all; 
@@ -6,7 +10,7 @@ use ieee.std_logic_arith.all;
 --Entity
 entity Deco1a4 is
 	--generic ( n: integer:=68; m: integer:=36; l: integer:=12; k: integer:=12; j: integer:=8); 
-	 
+	--It can be enabled for vectors of the bits to be encoded. 
 	port(	A: in bit_vector(67 downto 0);
 			en: in std_logic;
 			CE: out bit_vector(35 downto 0);
@@ -26,7 +30,8 @@ architecture solve of Deco1a4 is
 		 ED<= f(7 downto 0) when en='1';   
 		 PE<= f(31 downto 20) when en='1'; 	 
 		 AL<= f(19 downto 8) when en='1'; 
-		 
+
+		 --It can be enabled for vectors of the bits to be encoded. 
 		 --f<= A; --when en='1' else (others=>'0');
 		 --CE<= f(n-1 downto l+k+j) when en='1'; 
 		 --ED<= f(j-1 downto 0) when en='1';   
